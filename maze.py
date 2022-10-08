ebbf9c911f81059f688086e897c53986a1b1e350
Soh Mei Ling <meigemini.91@gmail.com>
2022-10-08 11:21:05 +0800

    update codes

--- maze.py
+++ maze.py
@@ -1,11 +1,12 @@
 from pygame import *
 
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (65, 65))
         self.speed = player_speed
-        self.rect = self.image.get_rect()d
+        self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
     def reset(self):
